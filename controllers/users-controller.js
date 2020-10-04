e171410ee46bd8faa0deec245d342a435d22308d
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-10-04 12:30:34 -0500

    create route for user

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -36,7 +36,14 @@ const userController = {
                 console.log(err);
                 res.status(400).json(err);
             });
-    }
+    },
+
+    // create a user
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
 
 
 
