77a2123d808df883f0babcffa5071e3fbc13ecd4
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-10-04 12:27:56 -0500

    created routes for finding all and one user

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -4,7 +4,38 @@ const userController = {
     // get all users
     getAllUsers(req, res) {
         User.find({})
-            .populate
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // get one user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .populate({
+                path: "thoughts",
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No User found with this ID!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
     }
 
 
